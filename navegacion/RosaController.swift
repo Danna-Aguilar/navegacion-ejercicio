9eb5a2e7ff4301f36c13948bad6be12e1add0870
Alumno <ulsa@D105.local>
2022-09-19 08:48:44 -0700

    fase1

---
+++ navegacion/RosaController.swift
@@ -0,0 +1,22 @@
+//
+//  RosaController.swift
+//  navegacion
+//
+//  Created by Alumno on 19/09/22.
+//  Copyright © 2022 Sebastian Encinas. All rights reserved.
+//
+
+import UIKit
+
+class RosaController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    @IBAction func doTapVolver(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ navegacion/VerdeController3.swift
@@ -0,0 +1,19 @@
+//
+//  VerdeController3.swift
+//  navegacion
+//
+//  Created by Alumno on 19/09/22.
+//  Copyright © 2022 Sebastian Encinas. All rights reserved.
+//
+
+
+import UIKit
+
+class VerdeController3: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+}
